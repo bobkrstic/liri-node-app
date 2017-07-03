9f31f126ed0c74162ab44282b3bd2476763888a5
Slobodan Krstic <slobodankrstic@Slobodans-MBP.wavecable.com>
2017-07-03 12:45:33 -0700

    initial commit

---
+++ keys.js
@@ -0,0 +1,8 @@
+console.log('this is loaded');
+
+exports.twitterKeys = {
+	consumer_key: 'HA7cTl2qrJC8sxEYDpt1wwtx1',
+	consumer_secret: 'Gf88ImIoBsq0tddfStEUbPWFI6QdFscIy8OmEbAFuSpNod7smF',
+	access_token_key: '66739501-nS0mfXIVQXn88fWP2SNRTkzsaBpObJ3VWii6rrkkF',
+	acces_token_secret: 'BvrbIYkjaUNhs8rqwOWvJfRiYkf2hAb1Qg7YV7xvvNgoP',
+}
---
+++ liri.js
@@ -0,0 +1,7 @@
+var twitterKeys = require("./keys.js");
+var request = require("request");
+var fs = require("fs");
+
+//var Twitter = require('twitter');
+
+console.log(twitterKeys.twitterKeys.consumer_key);
